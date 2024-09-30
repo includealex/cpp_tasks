08ec3b90c11bc852c894d8576a371e351f33f8c7
Aleksandr Mishin <70340829+includealex@users.noreply.github.com>
2024-09-30 05:37:32 +0300

    namespace fix (#4)
    Signed-off-by: Aleksandr Mishin <mishin.aa@phystech.edu>

--- 01-cowstring/tests/test_cow.cpp
+++ 01-cowstring/tests/test_cow.cpp
@@ -2,16 +2,14 @@
 
 #include "../src/my_str_COW.hpp"
 
-using namespace COW;
-
 TEST(MyStrCOWTest, DefaultConstructor) {
-    my_str_COW<char> str;
+    COW::my_str_COW<char> str;
     EXPECT_EQ(str.size(), 1);
     EXPECT_EQ(str[0], '\0');
 }
 
 TEST(MyStrCOWTest, ConstructorFromStringLiteral) {
-    my_str_COW<char> str("hello");
+    COW::my_str_COW<char> str("hello");
     EXPECT_EQ(str.size(), 5);
     EXPECT_EQ(str[0], 'h');
     EXPECT_EQ(str[1], 'e');
@@ -21,8 +19,8 @@ TEST(MyStrCOWTest, ConstructorFromStringLiteral) {
 }
 
 TEST(MyStrCOWTest, CopyConstructor) {
-    my_str_COW<char> original("copy");
-    my_str_COW<char> copy = original;
+    COW::my_str_COW<char> original("copy");
+    COW::my_str_COW<char> copy = original;
 
     EXPECT_EQ(copy.size(), 4);
     EXPECT_EQ(copy[0], 'c');
@@ -32,8 +30,8 @@ TEST(MyStrCOWTest, CopyConstructor) {
 }
 
 TEST(MyStrCOWTest, MoveConstructor) {
-    my_str_COW<char> original("move");
-    my_str_COW<char> moved = std::move(original);
+    COW::my_str_COW<char> original("move");
+    COW::my_str_COW<char> moved = std::move(original);
 
     EXPECT_EQ(moved.size(), 4);
     EXPECT_EQ(moved[0], 'm');
@@ -43,8 +41,8 @@ TEST(MyStrCOWTest, MoveConstructor) {
 }
 
 TEST(MyStrCOWTest, CopyAssignment) {
-    my_str_COW<char> str1("assignment");
-    my_str_COW<char> str2 = str1;
+    COW::my_str_COW<char> str1("assignment");
+    COW::my_str_COW<char> str2 = str1;
 
     EXPECT_EQ(str2.size(), 10);
     EXPECT_EQ(str2[0], 'a');
@@ -52,8 +50,8 @@ TEST(MyStrCOWTest, CopyAssignment) {
 }
 
 TEST(MyStrCOWTest, MoveAssignment) {
-    my_str_COW<char> str1("assignment");
-    my_str_COW<char> str2 = std::move(str1);
+    COW::my_str_COW<char> str1("assignment");
+    COW::my_str_COW<char> str2 = std::move(str1);
 
     EXPECT_EQ(str2.size(), 10);
     EXPECT_EQ(str2[0], 'a');
@@ -61,21 +59,21 @@ TEST(MyStrCOWTest, MoveAssignment) {
 }
 
 TEST(MyStrCOWTest, IndexOperatorConst) {
-    const my_str_COW<char> str("index");
+    const COW::my_str_COW<char> str("index");
     EXPECT_EQ(str[0], 'i');
     EXPECT_EQ(str[4], 'x');
     EXPECT_THROW(str[5], std::out_of_range);
 }
 
 TEST(MyStrCOWTest, IndexOperatorNonConst) {
-    my_str_COW<char> str("change");
+    COW::my_str_COW<char> str("change");
     EXPECT_EQ(str[0], 'c');
     str[0] = 'C';
     EXPECT_EQ(str[0], 'C');
 }
 
 TEST(MyStrCOWTest, Substring) {
-    my_str_COW<char> str("substring");
+    COW::my_str_COW<char> str("substring");
     auto sub = str.substr(3, 5);
     EXPECT_EQ(sub.size(), 3);
     EXPECT_EQ(sub[0], 's');
@@ -85,8 +83,8 @@ TEST(MyStrCOWTest, Substring) {
 }
 
 TEST(MyStrCOWTest, Concatenation) {
-    my_str_COW<char> str1("hello ");
-    my_str_COW<char> str2("world");
+    COW::my_str_COW<char> str1("hello ");
+    COW::my_str_COW<char> str2("world");
     auto concat = str1.concat(str2);
     EXPECT_EQ(concat.size(), 11);
     EXPECT_EQ(concat[0], 'h');
@@ -96,20 +94,20 @@ TEST(MyStrCOWTest, Concatenation) {
 }
 
 TEST(MyStrCOWTest, Search) {
-    my_str_COW<char> str("hello world");
-    my_str_COW<char> substr("world");
+    COW::my_str_COW<char> str("hello world");
+    COW::my_str_COW<char> substr("world");
     auto positions = str.search(substr);
     EXPECT_EQ(positions.size(), 1);
     EXPECT_EQ(positions[0], 6);
 
-    my_str_COW<char> l ("l");
+    COW::my_str_COW<char> l ("l");
     auto l_num = str.search(l);
     EXPECT_EQ(l_num.size(), 3);
 }
 
 TEST(MyStrCOWTest, Tokenization) {
-    my_str_COW<char> str("tokenize,this,string");
-    my_str_COW<char> delimiter(",");
+    COW::my_str_COW<char> str("tokenize,this,string");
+    COW::my_str_COW<char> delimiter(",");
     auto tokens = str.tokenize(delimiter);
 
     EXPECT_EQ(tokens.size(), 3);
